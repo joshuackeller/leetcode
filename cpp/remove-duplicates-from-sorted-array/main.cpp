1093141961c19b2fa8be7c3ac028dc16550d5269
joshuackeller <joshuackeller@gmail.com>
2024-01-09 22:45:25 -0700

    remove duplicates c++

--- cpp/remove-duplicates-from-sorted-array/main.cpp
+++ cpp/remove-duplicates-from-sorted-array/main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <vector>
+
+//  g++ -std=c++11 main.cpp && ./a.out
+
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int uniqueIndex = 0;
+
+        vector<int> numsCopy = nums;
+        for (int i = 1; i < numsCopy.size(); i++)
+        {
+            if (nums[uniqueIndex] != numsCopy[i])
+            {
+                uniqueIndex++;
+                nums.insert(nums.begin() + uniqueIndex, numsCopy[i]);
+            }
+        }
+
+        return uniqueIndex + 1;
+    }
+};
 
 int main()
 {
-    std::cout << "Asuh dude";
+    Solution solution;
+
+    vector<int> nums = {0, 1, 2, 2, 2, 3, 7, 8, 8, 9};
+    cout << solution.removeDuplicates(nums);
+
     return 0;
 }
