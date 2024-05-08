fbb78cd086abd2f5bfa2ac14199efeab602d46da
joshuackeller <joshuackeller@gmail.com>
2024-05-08 08:30:30 -0600

    implement stack using queues and number of students unable to eat lunch

---
+++ python/number-of-students-unable-to-eat-lunch/main.py
@@ -0,0 +1,26 @@
+from types import List
+
+
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        st_c = 0
+        st_s = 0
+        for st in students:
+            if st == 0:
+                st_c += 1
+            else:
+                st_s += 1
+
+        for sw in sandwiches:
+            if sw == 0:
+                if st_c == 0:
+                    return st_s
+                else:
+                    st_c -= 1
+            elif sw == 1:
+                if st_s == 0:
+                    return st_c
+                else:
+                    st_s -= 1
+
+        return st_c + st_s
