57b6808292f657222b6afda14c391889804211b7
joshuackeller <joshuackeller@gmail.com>
2024-05-09 20:11:36 -0600

    reverse linked list

---
+++ python/reverse-linked-list/main.py
@@ -0,0 +1,32 @@
+from types import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+
+        return prev
+
+        # if not head:
+        #     return
+
+        # node = None
+        # while head:
+        #     node = ListNode(head.val, node)
+        #     head = head.next
+
+        # return node
